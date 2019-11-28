a9e1eb33b0676064107a896edb78b07e0e758c72
ximaxer <ximaxer@users.github.com>
2019-11-28 04:26:10 +0000

    dc cc

---
+++ src/main/java/InvestigationCenter.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+public class InvestigationCenter {
+    
+   
+    public static void main(String[] args) {
+        ArrayList<Project> Projects = new ArrayList<Project>() ;
+    }
+    
+}
