bfb8d4b9ca06231af4d625087e04aabb9fddf623
ximaxer <ximaxer@users.github.com>
2019-11-28 18:06:50 +0000

    pipi
    teta

--- src/main/java/InvestigationCenter.java
+++ src/main/java/InvestigationCenter.java
@@ -1,9 +1,42 @@
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.Calendar;
+
 public class InvestigationCenter {
+    //variables
+    ArrayList<Project> projects = new ArrayList<Project>();
     
-   
-    public static void main(String[] args) {
-        ArrayList<Project> Projects = new ArrayList<Project>() ;
+    
+    //constructor
+    public InvestigationCenter(){
+        //?
     }
     
-}adwsedfghbn
+    //methods
+    public void showDelayedP(){
+        Project A = new Project();
+        println("\nPROJETOS ATRASADOS:");
+        for(int i=0;i<projects.size();i++){
+            A = Projects.get(i);
+            
+            Calendar currentT = Calendar.getInstance();
+            Calendar est = Calendar.setTime(A.date);
+            est= Calendar.getInstance(A.date);
+            est.add(Calendar.MONTH , A.estDuration);
+            Date dataEstFinal = est.getTime();    //dataEstFinal -> data inicial + estimated duration
+            Date dataHoje = currentT.getTime();
+            
+            
+            if(dataHoje.compareTo(dataEstFinal)==1){
+                printf("\nProjeto: %s", A.name);
+            }               
+        }
+    }
+    public void showFinishedP(){
+        
+    }
+    public static void main(String[] args) {
+        
+    }
+    
+}
