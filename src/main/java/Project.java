51582e39bfc5d40edd95af0a8aaf9c866e2394c5
ximaxer <ximaxer@users.github.com>
2019-12-09 04:57:20 +0000

    eu gosto
    da Tothianna Simões :100:

--- src/main/java/Project.java
+++ src/main/java/Project.java
@@ -1,4 +1,5 @@
-import java.util.ArrayList;
+import java.io.*;
+import java.text.*;
 import java.util.*;
 public class Project extends InvestigationCenter {
     //========================================================================//
@@ -33,12 +34,13 @@ public class Project extends InvestigationCenter {
         this.projectState=true;
     }
 
-    public Project(String name,String acronym,Date initDate,int estDuration,Date finalDate){
+    public Project(String name,String acronym,Date initDate,int estDuration){
         this.name=name;
         this.acronym=acronym;
         this.initialDate=initDate;
         this.estDuration=estDuration;
         this.finalDate=null;
+        this.projectState=true;
 
     }
     
@@ -83,6 +85,132 @@ public class Project extends InvestigationCenter {
     public void setFinalDate(Date finalDate){
         this.finalDate=finalDate;
     }
+    
+        
+    public void ReadTasksFromFile(){
+        BufferedReader reader;
+        
+        int i = 0, estDuration = 0;
+        String type = "", name = "";
+        Date date = new Date(0,0,0);
+        
+        File f = new File("C:\\Users\\ximax\\Desktop\\college gomin\\POO\\ProjetoPoo\\Task_list.txt");
+        if(f.exists() && f.isFile()){
+            try{
+                reader = new BufferedReader(new FileReader(f));
+                    String line = reader.readLine();
+                    while (line != null) {        
+                        String[] tokens = line.split("\t");
+                           i=0;
+                        for (String t : tokens){
+                            i++;
+                            switch(i){
+                                case 1:
+                                    type = t;
+                                    break;
+                                case 2:
+                                    name = t;
+                                    break;
+                                case 3:
+                                    estDuration= Integer.parseInt(t);
+                                    break;
+                                case 4:
+                                    try{
+                                        date=new SimpleDateFormat("dd/MM/yyyy").parse(t);
+                                    }catch(ParseException e){
+                                        e.printStackTrace();
+                                    }
+                                    break;
+                                default:
+                                    break;
+                            } 
+                        }
+                        if(type.compareTo("Design")==0){
+                            Design task = new Design(name, date, estDuration);
+                            tasks.add(task);
+                        }else if(type.compareTo("Development")==0){
+                            Development task = new Development(name, date, estDuration);
+                            tasks.add(task);
+                        }else if(type.compareTo("Documentation")==0){
+                            Documentation task = new Documentation(name, date, estDuration);
+                            tasks.add(task);
+                        }
+                        line = reader.readLine();
+                        //le a proxima linha e passa à proxime iteração                            
+                        //do ciclo com a nova linha
+                        //de forma a garantir que a linha não é nula
+                    }
+                    reader.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+public void ReadPersonsFromFile(){
+        BufferedReader reader;
+        
+        int i = 0, numMec = 0;
+        String type = "", name = "", email = "", area = "";
+        
+        File personf = new File("C:\\Users\\ximax\\Desktop\\college gomin\\POO\\ProjetoPoo\\Person_list.txt");
+        if(personf.exists() && personf.isFile()){
+            try{
+                reader = new BufferedReader(new FileReader(personf));
+                    String line = reader.readLine();
+                    while (line != null) {        
+                        String[] tokens = line.split("\t");
+                           i=0;
+                        for (String t : tokens){
+                            i++;
+                            switch(i){
+                                case 1:
+                                    type = t;
+                                    break;
+                                case 2:
+                                    name = t;
+                                    break;
+                                case 3:
+                                    email= t;
+                                    break;
+                                case 4:
+                                    numMec= Integer.parseInt(t);
+                                    break;
+                                case 5:
+                                    area = t;
+                                    break;
+                                default:
+                                    break;
+                            } 
+                        }
+                        if(type.compareTo("Graduate")==0){
+                            Graduate grantee = new Graduate(name, email);
+                            grantees.add(grantee);
+                        }else if(type.compareTo("Doctorate")==0){
+                            Doctorate grantee = new Doctorate(name, email);
+                            grantees.add(grantee);
+                        }else if(type.compareTo("Master")==0){
+                            Master grantee = new Master(name, email);
+                            grantees.add(grantee);
+                        }else if(type.compareTo("Docent")==0){
+                            Docent docent = new Docent(name, email, numMec, area);
+                            docents.add(docent);
+                        }
+                        line = reader.readLine();
+                        //le a proxima linha e passa à proxime iteração                            
+                        //do ciclo com a nova linha
+                        //de forma a garantir que a linha não é nula
+                    }
+                    reader.close();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+}
+
+    
+    
     public void deleteTask(int taskI){
         tasks.remove(taskI);
         System.out.printf("\nTask %d deleted with success!", taskI); 
@@ -94,7 +222,7 @@ public class Project extends InvestigationCenter {
         }
     }
     public void showUnitializedTasks(){
-        System.out.println("\nLISISTING UNITIALIZED TASKS");
+        System.out.println("\nLISTING UNITIALIZED TASKS");
         for(int i=0;i<tasks.size();i++){
             if(tasks.get(i).getProgress()==0){
                 System.out.printf("\nTask %s not initalized", tasks.get(i).getTName());
