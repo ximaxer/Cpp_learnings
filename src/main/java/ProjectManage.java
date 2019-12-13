6590270fadff8bf3ed1b36ed864804b6917b43b1
Tatyplays <Tatyplays@github.com>
2019-12-13 21:16:41 +0000

    sdfg

--- src/main/java/ProjectManage.java
+++ src/main/java/ProjectManage.java
@@ -12,11 +12,11 @@ public class ProjectManage {
     private Project projeto;
     private JLabel label;
     private JPanel panel= new JPanel();
-    private JFrame f = new JFrame("Manage Project");
-    ArrayList<Task> gTaskList;
-    private DefaultListModel listaTasks;
+    private JFrame f = new JFrame("Manage Project"), f3 = new JFrame("list completed");
+    ArrayList<Task> gTaskList,copyTaskList=new ArrayList<Task>() ;
+    private DefaultListModel listaTasks, compTasks;
     private JList taskList;
-    private JButton finalize, add, remove;
+    private JButton finalize, add, remove,assignTask,listCompTasks,listNotInitTasks,listDelayedTasks;
     
     public ProjectManage(Project proj) {
         projeto=proj;
@@ -41,21 +41,53 @@ public class ProjectManage {
         remove = new JButton("remove task");
         remove.setActionCommand("remove task");
         remove.addActionListener(new RemoveListener());
+        
+        assignTask = new JButton("assign task");
+        assignTask.setActionCommand("assign ask");
+        //assignTask.addActionListener(new AssignListener());
+        
+        listCompTasks = new JButton("list completed");
+        listCompTasks.setActionCommand("list completed");
+        listCompTasks.addActionListener(new ListCompTasksListener());
+        
+        listNotInitTasks = new JButton("list not initialized");
+        listNotInitTasks.setActionCommand("list not initialized");
+        //listNotInitTasks.addActionListener(new ListNotInitTasksListener());
+        
+        listDelayedTasks = new JButton("list delayed");
+        listDelayedTasks.setActionCommand("list delayed");
+        //listDelayedTasks.addActionListener(new ListDelayedTasksListener());
        
         //Create a panel that uses BoxLayout.
-        JPanel buttonPane = new JPanel();
-        buttonPane.setLayout(new BoxLayout(buttonPane,
+        JPanel buttonPane1 = new JPanel();
+        buttonPane1.setLayout(new BoxLayout(buttonPane1,
                                            BoxLayout.LINE_AXIS));
-        buttonPane.add(finalize);
-        buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
-        buttonPane.add(add);
-        buttonPane.add(new JSeparator(SwingConstants.VERTICAL));
-        buttonPane.add(remove);
-        buttonPane.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        buttonPane1.add(finalize);
+        buttonPane1.add(new JSeparator(SwingConstants.VERTICAL));
+        buttonPane1.add(add);
+        buttonPane1.add(new JSeparator(SwingConstants.VERTICAL));
+        buttonPane1.add(remove);
+        buttonPane1.add(new JSeparator(SwingConstants.VERTICAL));
+        buttonPane1.add(assignTask);
+        buttonPane1.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        
+        JPanel buttonPane2 = new JPanel();
+        buttonPane2.setLayout(new BoxLayout(buttonPane2,
+                                           BoxLayout.LINE_AXIS));
+        buttonPane2.add(new JSeparator(SwingConstants.VERTICAL));
+        buttonPane2.add(listCompTasks);
+        buttonPane2.add(new JSeparator(SwingConstants.VERTICAL));
+        buttonPane2.add(listNotInitTasks);
+        buttonPane2.add(new JSeparator(SwingConstants.VERTICAL));
+        buttonPane2.add(listDelayedTasks);
+        buttonPane2.setBorder(BorderFactory.createEmptyBorder(5,5,5,5));
+        
+        
 
         f.add(listScrollPane, BorderLayout.CENTER);
-        f.add(buttonPane, BorderLayout.PAGE_END);
-        f.setSize(350,350);
+        f.add(buttonPane1, BorderLayout.PAGE_START);
+        f.add(buttonPane2, BorderLayout.PAGE_END);
+        f.setSize(450,450);
         f.setVisible(true);
         f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
        
@@ -79,7 +111,7 @@ public class ProjectManage {
             }
             }
             catch(IllegalArgumentException ex){
-            JOptionPane.showMessageDialog(null,"Error, task is not 100% completed!","Error",JOptionPane.INFORMATION_MESSAGE);;
+            JOptionPane.showMessageDialog(null,"Error, task is not 100% completed!","Error",JOptionPane.INFORMATION_MESSAGE);
 
             }
         }
@@ -98,6 +130,65 @@ public class ProjectManage {
                  listaTasks.remove(index); 
                  panel.updateUI();
                  gTaskList.remove(index);  
-             }
         }
+    
+    }
+    /*private class AssignListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+           
+                
+        }
+    
+    }*/
+    private class ListCompTasksListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+            try{
+                for(int i=0;i<gTaskList.size();i++){
+                    if(gTaskList.get(i).getProgress()==100){
+                        copyTaskList.add(gTaskList.get(i));
+                        
+                    }
+                }
+                if(copyTaskList.size()!=0){
+                    
+                    compTasks = new DefaultListModel();
+                    for(int j=0;j<copyTaskList.size();j++){
+                        compTasks.addElement(copyTaskList.get(j).getTName());
+                    }
+                    taskList = new JList(compTasks);
+                    //JScrollPane listScrollPane = new JScrollPane(taskList);
+                    f3.add(taskList, BorderLayout.CENTER);
+                    f3.setSize(300,300);
+                    f3.setVisible(true);
+                    f3.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+                }
+                else{
+                    throw new IllegalArgumentException();
+                }
+            }
+            catch(IllegalArgumentException ex){
+            JOptionPane.showMessageDialog(null,"Error, there are no completed tasks yet!","Error",JOptionPane.INFORMATION_MESSAGE);
+            }
+        }
+    
+    }
+    private class ListNotInitTasksListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+
+
+        }
+
+    }
+    /*
+    private class ListDelayedTasksListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent e){
+
+
+        }
+
+    }*/
 }
